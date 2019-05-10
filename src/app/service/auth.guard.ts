44f086f45866be4b4ac943b4f822a7e81f9c3082
mateusbw <mateus_bwilding@hotmail.com>
2019-05-10 13:14:35 -0300

    Auth

---
+++ src/app/service/auth.guard.ts
@@ -0,0 +1,14 @@
+
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate{
+
+    constructor(private authService: AuthService){};
+
+    canActivate(): boolean {
+        return this.authService.autenticado();
+    };
+}
