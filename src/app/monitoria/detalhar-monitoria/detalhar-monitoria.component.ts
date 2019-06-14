8fb3654f6a3e5fd8927875fa37a4059b5263476e
João Vitor <joaovitor.felipesantos@gmail.com>
2019-06-14 15:06:16 -0300

    detalhar monitoria

--- src/app/monitoria/detalhar-monitoria/detalhar-monitoria.component.ts
+++ src/app/monitoria/detalhar-monitoria/detalhar-monitoria.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MonitoriaService } from 'src/app/service/monitoria.service';
+import { MessageService } from 'src/app/core/message/message.service';
 
 @Component({
   selector: 'app-detalhar-monitoria',
@@ -11,12 +12,18 @@ export class DetalharMonitoriaComponent implements OnInit {
   public dadosMonitoria = {};
   public monitoriaId;
   constructor(route: ActivatedRoute,
-              private monitoriaService: MonitoriaService) {
+              private monitoriaService: MonitoriaService,
+              private messageService: MessageService) {
       this.monitoriaId = route.snapshot.params['id'];
    }
 
   ngOnInit() {
     console.log(this.monitoriaId);
+    this.monitoriaService.buscarDetalhesMonitoria(this.monitoriaId).subscribe(data=>{
+      this.dadosMonitoria = data;
+    }, error =>{
+      this.messageService.addMsgDanger(error.error.message);
+    });
   }
 
 }
