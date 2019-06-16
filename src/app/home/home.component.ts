c7308fe7a3e2470bf5d53496a76130242d52d550
Josias <josiassilva32@gmail.com>
2019-06-16 13:39:37 -0300

    validação de opção do menu por role

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import {AuthService} from 'src/app/service/auth.service'
 import { SecurityService } from '../core/security/security.service';
 import { MessageService } from '../core/message/message.service'
+import { not } from '@angular/compiler/src/output/output_ast';
 
 /**
  * Implementação do component 'Home' responsável por prover o template padrão da aplicação.
@@ -35,10 +36,14 @@ export class HomeComponent {
       }
 
       this.router.events.subscribe((ev) => {
+        
         if (ev instanceof NavigationEnd) { 
           /* Your code goes here on every router change */
-          this.rota = this.router.url;
+          this.rota = this.router.url; 
           
+          if (!this.verificaAcessosRota()) {            
+            //this.router.navigate(['/']);
+          }       
         }
       });
       
@@ -48,21 +53,53 @@ export class HomeComponent {
       console.log(this.usuario.role);
       if(this.usuario.role == "Aluno"){
         this.acessos = ["ativas"];
-      }
+      }else
       if(this.usuario.role == "Monitor"){
         this.acessos = ["preenche_diario", "alunos"];
+      }else
+      if(this.usuario.role == "Professor"){
+        this.acessos = ["monitoria_realizada", "ativas"];
+      }else
+      if(this.usuario.role == "Coordenador"){
+        this.acessos = ["monitoria_realizada", "ativas","historico"];
+      }else
+      if(this.usuario.role == "Coordenação"){
+        this.acessos = ["monitoria","historico"];
       }
       console.log(this.acessos);
       console.log(this.usuario);
 
+      //rotas por role
+      if(this.usuario.role == "Aluno"){
+        this.acessos = ["/monitoria/listar-ativas"];
+      }else
+      if(this.usuario.role == "Monitor"){
+        this.acessos = ["preenche_diario", "alunos","ativas"];
+      }else
+      if(this.usuario.role == "Professor"){
+        this.acessos = ["monitoria_realizada", "ativas"];
+      }else
+      if(this.usuario.role == "Coordenador"){
+        this.acessos = ["monitoria_realizada", "ativas","historico"];
+      }else
+      if(this.usuario.role == "Coordenação"){
+        this.acessos = ["monitoria","historico"];
+      }
+
     }
 
     verificaAcessos(acesso){
-      console.log(acesso)
       for(let a of this.acessos){
-        console.log(a);
         if (a == acesso){
-          console.log("Ok")
+          return true;
+        }
+      }
+      return false;
+    }
+
+    verificaAcessosRota(){
+      for(let a of this.acessoRotas){
+        if (a == this.rota){
           return true;
         }
       }
